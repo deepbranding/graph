a8144a62b59c1deb917d5ed6755d0a1f2bbd6a7b
eduar-code <77644968+eduar-calm@users.noreply.github.com>
2025-03-07 00:30:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,65 +4,58 @@ const elts = {
 };
 
 const svgPaths = [
-    "svgs/Sin título-1-01.svg",
-    "svgs/Sin título-1-02.svg",
-    "svgs/Sin título-1-03.svg",
-    "svgs/Sin título-1-04.svg",
-    "svgs/Sin título-1-05.svg",
-    "svgs/Sin título-1-06.svg",
-    "svgs/Sin título-1-07.svg",
-    "svgs/Sin título-1-08.svg",
-    "svgs/Sin título-1-09.svg",
-    "svgs/Sin título-1-10.svg",
-    "svgs/Sin título-1-11.svg",
-    "svgs/Sin título-1-12.svg",
-    "svgs/Sin título-1-13.svg",
-    "svgs/Sin título-1-14.svg",
-    "svgs/Sin título-1-15.svg",
-    "svgs/Sin título-1-16.svg",
-    "svgs/Sin título-1-17.svg",
-    "svgs/Sin título-1-18.svg",
-    "svgs/Sin título-1-19.svg",
-    "svgs/Sin título-1-20.svg",
-    "svgs/Sin título-1-21.svg",
-    "svgs/Sin título-1-22.svg",
-    "svgs/Sin título-1-23.svg",
-    "svgs/Sin título-1-24.svg",
-    "svgs/Sin título-1-25.svg",
-    "svgs/Sin título-1-26.svg",
-    "svgs/Sin título-1-27.svg",
-    "svgs/Sin título-1-28.svg",
-    "svgs/Sin título-1-29.svg",
-    "svgs/Sin título-1-30.svg",
-    "svgs/Sin título-1-31.svg",
+
+	"svgs/Sin título-1-01.svg",
+"svgs/Sin título-1-02.svg",
+"svgs/Sin título-1-03.svg",
+"svgs/Sin título-1-04.svg",
+"svgs/Sin título-1-05.svg",
+"svgs/Sin título-1-06.svg",
+"svgs/Sin título-1-07.svg",
+"svgs/Sin título-1-08.svg",
+"svgs/Sin título-1-09.svg",
+"svgs/Sin título-1-10.svg",
+"svgs/Sin título-1-11.svg",
+"svgs/Sin título-1-12.svg",
+"svgs/Sin título-1-13.svg",
+"svgs/Sin título-1-14.svg",
+"svgs/Sin título-1-15.svg",
+"svgs/Sin título-1-16.svg",
+"svgs/Sin título-1-17.svg",
+"svgs/Sin título-1-18.svg",
+"svgs/Sin título-1-19.svg",
+"svgs/Sin título-1-20.svg",
+"svgs/Sin título-1-21.svg",
+"svgs/Sin título-1-22.svg",
+"svgs/Sin título-1-23.svg",
+"svgs/Sin título-1-24.svg",
+"svgs/Sin título-1-25.svg",
+"svgs/Sin título-1-26.svg",
+"svgs/Sin título-1-27.svg",
+"svgs/Sin título-1-28.svg",
+"svgs/Sin título-1-29.svg",
+"svgs/Sin título-1-30.svg",
+"svgs/Sin título-1-31.svg",
 ];
 
 const morphTime = 1;
 const cooldownTime = 0.25;
 
-let svgIndex = 0;
+let svgIndex = svgPaths.length - 1;
 let time = new Date();
 let morph = 0;
 let cooldown = cooldownTime;
-let isLoading = false;
 
-// Carga un SVG y lo asigna solo cuando esté listo
 function loadSVG(element, path) {
-    isLoading = true;
     fetch(path)
         .then(response => response.text())
         .then(data => {
             element.innerHTML = data;
-            isLoading = false;
         })
-        .catch(error => {
-            console.error("Error loading SVG:", error);
-            isLoading = false;
-        });
+        .catch(error => console.error("Error loading SVG:", error));
 }
 
-// Carga los dos primeros SVGs para iniciar sin parpadeo
-loadSVG(elts.text1, svgPaths[svgIndex]);
+loadSVG(elts.text1, svgPaths[svgIndex % svgPaths.length]);
 loadSVG(elts.text2, svgPaths[(svgIndex + 1) % svgPaths.length]);
 
 function doMorph() {
@@ -99,19 +92,20 @@ function animate() {
     requestAnimationFrame(animate);
 
     let newTime = new Date();
+    let shouldIncrementIndex = cooldown > 0;
     let dt = (newTime - time) / 1000;
     time = newTime;
 
     cooldown -= dt;
 
     if (cooldown <= 0) {
-        nextSVG();
-        cooldown = cooldownTime; // Resetear el cooldown
-    }
-
-    doMorph();
-}
- else {
+        if (shouldIncrementIndex) {
+            svgIndex++;
+            loadSVG(elts.text1, svgPaths[svgIndex % svgPaths.length]);
+            loadSVG(elts.text2, svgPaths[(svgIndex + 1) % svgPaths.length]);
+        }
+        doMorph();
+    } else {
         doCooldown();
     }
 }
