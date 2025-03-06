cbd7f38549f29f3d94b49bc3cce063ddd3031b59
eduar-code <77644968+eduar-calm@users.noreply.github.com>
2025-03-06 23:26:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
 const elts = {
-    text1: document.getElementById("text1"),
-    text2: document.getElementById("text2")
+    container: document.getElementById("container")
 };
 
 const svgPaths = [
-
-	"svgs/Sin título-1-01.svg",
-"svgs/Sin título-1-02.svg",
-"svgs/Sin título-1-03.svg",
-"svgs/Sin título-1-04.svg",
-"svgs/Sin título-1-05.svg",
-"svgs/Sin título-1-06.svg",
-"svgs/Sin título-1-07.svg",
-"svgs/Sin título-1-08.svg",
-"svgs/Sin título-1-09.svg",
-"svgs/Sin título-1-10.svg",
-"svgs/Sin título-1-11.svg",
-"svgs/Sin título-1-12.svg",
-"svgs/Sin título-1-13.svg",
-"svgs/Sin título-1-14.svg",
-"svgs/Sin título-1-15.svg",
-"svgs/Sin título-1-16.svg",
-"svgs/Sin título-1-17.svg",
-"svgs/Sin título-1-18.svg",
-"svgs/Sin título-1-19.svg",
-"svgs/Sin título-1-20.svg",
-"svgs/Sin título-1-21.svg",
-"svgs/Sin título-1-22.svg",
-"svgs/Sin título-1-23.svg",
-"svgs/Sin título-1-24.svg",
-"svgs/Sin título-1-25.svg",
-"svgs/Sin título-1-26.svg",
-"svgs/Sin título-1-27.svg",
-"svgs/Sin título-1-28.svg",
-"svgs/Sin título-1-29.svg",
-"svgs/Sin título-1-30.svg",
-"svgs/Sin título-1-31.svg",
+    "svgs/Sin título-1-01.svg",
+    "svgs/Sin título-1-02.svg",
+    "svgs/Sin título-1-03.svg",
+    "svgs/Sin título-1-04.svg",
+    "svgs/Sin título-1-05.svg",
+    "svgs/Sin título-1-06.svg",
+    "svgs/Sin título-1-07.svg",
+    "svgs/Sin título-1-08.svg",
+    "svgs/Sin título-1-09.svg",
+    "svgs/Sin título-1-10.svg",
+    "svgs/Sin título-1-11.svg",
+    "svgs/Sin título-1-12.svg",
+    "svgs/Sin título-1-13.svg",
+    "svgs/Sin título-1-14.svg",
+    "svgs/Sin título-1-15.svg",
+    "svgs/Sin título-1-16.svg",
+    "svgs/Sin título-1-17.svg",
+    "svgs/Sin título-1-18.svg",
+    "svgs/Sin título-1-19.svg",
+    "svgs/Sin título-1-20.svg",
+    "svgs/Sin título-1-21.svg",
+    "svgs/Sin título-1-22.svg",
+    "svgs/Sin título-1-23.svg",
+    "svgs/Sin título-1-24.svg",
+    "svgs/Sin título-1-25.svg",
+    "svgs/Sin título-1-26.svg",
+    "svgs/Sin título-1-27.svg",
+    "svgs/Sin título-1-28.svg",
+    "svgs/Sin título-1-29.svg",
+    "svgs/Sin título-1-30.svg",
+    "svgs/Sin título-1-31.svg",
 ];
 
 const morphTime = 1;
@@ -46,19 +44,25 @@ let time = new Date();
 let morph = 0;
 let cooldown = cooldownTime;
 
-function loadSVG(element, path, callback) {
+let currentSVG = document.createElement("div");
+let nextSVG = document.createElement("div");
+currentSVG.classList.add("svg-container");
+nextSVG.classList.add("svg-container");
+
+elts.container.appendChild(currentSVG);
+elts.container.appendChild(nextSVG);
+
+function loadSVG(element, path) {
     fetch(path)
         .then(response => response.text())
         .then(data => {
             element.innerHTML = data;
-            if (callback) callback(); // Llamar al callback cuando se haya cargado el SVG
         })
         .catch(error => console.error("Error loading SVG:", error));
 }
 
-
-loadSVG(elts.text1, svgPaths[svgIndex % svgPaths.length]);
-loadSVG(elts.text2, svgPaths[(svgIndex + 1) % svgPaths.length]);
+loadSVG(currentSVG, svgPaths[svgIndex % svgPaths.length]);
+loadSVG(nextSVG, svgPaths[(svgIndex + 1) % svgPaths.length]);
 
 function doMorph() {
     morph -= cooldown;
@@ -74,20 +78,20 @@ function doMorph() {
 }
 
 function setMorph(fraction) {
-    elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-    elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+    nextSVG.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+    nextSVG.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
 
     fraction = 1 - fraction;
-    elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-    elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+    currentSVG.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+    currentSVG.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
 }
 
 function doCooldown() {
     morph = 0;
-    elts.text2.style.filter = "";
-    elts.text2.style.opacity = "100%";
-    elts.text1.style.filter = "";
-    elts.text1.style.opacity = "0%";
+    nextSVG.style.filter = "";
+    nextSVG.style.opacity = "100%";
+    currentSVG.style.filter = "";
+    currentSVG.style.opacity = "0%";
 }
 
 function animate() {
@@ -103,16 +107,17 @@ function animate() {
     if (cooldown <= 0) {
         if (shouldIncrementIndex) {
             svgIndex++;
-            loadSVG(elts.text1, svgPaths[svgIndex % svgPaths.length], () => {
-                loadSVG(elts.text2, svgPaths[(svgIndex + 1) % svgPaths.length], doMorph);
-            });
-        } else {
-            doMorph();
+
+            let temp = currentSVG;
+            currentSVG = nextSVG;
+            nextSVG = temp;
+
+            loadSVG(nextSVG, svgPaths[(svgIndex + 1) % svgPaths.length]);
         }
+        doMorph();
     } else {
         doCooldown();
     }
 }
-
 
 animate();
