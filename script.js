90731a126a0cab0ca58e38aa01b14e2a0ab5e877
eduar-code <77644968+eduar-calm@users.noreply.github.com>
2025-03-07 01:53:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,32 @@ const svgPaths = [
     "svgs/Sin título-1-03.svg",
     "svgs/Sin título-1-04.svg",
     "svgs/Sin título-1-05.svg",
-    // ... tus otros SVGs
+    "svgs/Sin título-1-06.svg",
+    "svgs/Sin título-1-07.svg",
+    "svgs/Sin título-1-08.svg",
+    "svgs/Sin título-1-09.svg",
+    "svgs/Sin título-1-10.svg",
+    "svgs/Sin título-1-11.svg",
+    "svgs/Sin título-1-12.svg",
+    "svgs/Sin título-1-13.svg",
+    "svgs/Sin título-1-14.svg",
+    "svgs/Sin título-1-15.svg",
+    "svgs/Sin título-1-16.svg",
+    "svgs/Sin título-1-17.svg",
+    "svgs/Sin título-1-18.svg",
+    "svgs/Sin título-1-19.svg",
+    "svgs/Sin título-1-20.svg",
+    "svgs/Sin título-1-21.svg",
+    "svgs/Sin título-1-22.svg",
+    "svgs/Sin título-1-23.svg",
+    "svgs/Sin título-1-24.svg",
+    "svgs/Sin título-1-25.svg",
+    "svgs/Sin título-1-26.svg",
+    "svgs/Sin título-1-27.svg",
+    "svgs/Sin título-1-28.svg",
+    "svgs/Sin título-1-29.svg",
+    "svgs/Sin título-1-30.svg",
+    "svgs/Sin título-1-31.svg",
 ];
 
 const morphTime = 1;
@@ -20,13 +45,20 @@ let time = new Date();
 let morph = 0;
 let cooldown = cooldownTime;
 
+// Función para cargar SVGs y asegurar que estén completamente disponibles
 function loadSVG(element, path) {
-    // Limpia el contenido antes de cargar el nuevo SVG
-    element.innerHTML = '';
     fetch(path)
         .then(response => response.text())
         .then(data => {
             element.innerHTML = data;
+
+            // Forzamos una actualización del DOM
+            requestAnimationFrame(() => {
+                // Aquí podemos empezar a animar después de que el SVG esté completamente cargado
+                if (path === svgPaths[svgIndex % svgPaths.length]) {
+                    doMorph();  // Llamamos a la animación de morphing una vez cargado el SVG
+                }
+            });
         })
         .catch(error => console.error("Error loading SVG:", error));
 }
@@ -77,7 +109,6 @@ function animate() {
     if (cooldown <= 0) {
         if (shouldIncrementIndex) {
             svgIndex++;
-            // Asegúrate de que los SVG se cargan correctamente sin parpadeos
             loadSVG(elts.text1, svgPaths[svgIndex % svgPaths.length]);
             loadSVG(elts.text2, svgPaths[(svgIndex + 1) % svgPaths.length]);
         }
@@ -88,4 +119,5 @@ function animate() {
 }
 
 animate();
+
 
